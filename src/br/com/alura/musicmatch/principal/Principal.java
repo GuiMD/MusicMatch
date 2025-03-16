74b6466f0cf236c496524477551b444aab146b8d
Guilherme.MD <gui.martins.duarte@gmail.com>
2025-03-16 20:01:29 -0300

    Testing the functionality of the 'Main' class by using a 'for loop' to simulate user likes.

--- src/br/com/alura/musicmatch/principal/Principal.java
+++ src/br/com/alura/musicmatch/principal/Principal.java
@@ -1,17 +1,32 @@
 package br.com.alura.musicmatch.principal;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.alura.musicmatch.modelos.Musica;
+import br.com.alura.musicmatch.modelos.Podcast;
+
 public class Principal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Musica musica1 = new Musica();
+        musica1.setTitulo("Forever");
+        musica1.setCantor("Kiss");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 1000; i++) {
+            musica1.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+
+        }
+
+        Podcast podcast1 = new Podcast();
+        podcast1.setTitulo("BolhaDev");
+        podcast1.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            podcast1.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            podcast1.curte();
         }
     }
 }
